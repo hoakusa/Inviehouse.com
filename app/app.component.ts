58644fa3108919aaa04416d5495e5f9b55c6471d
hoakusa <hoakusa@gmail.com>
2016-12-13 15:24:36 +0200

    sectio

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html',
+    styleUrls: ['./app/css/app.less'],
 })
 export class AppComponent { }
